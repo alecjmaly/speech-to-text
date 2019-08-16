eebd5cb5e30caca339a8d4237d799322c3dc0d82
Alec Maly <32174181+alecjmaly@users.noreply.github.com>
2019-08-16 16:15:11 -0400

    Add files via upload

---
+++ extract speech from google response.ps1
@@ -0,0 +1,20 @@
+﻿## set variables
+$input_file = "$($env:USERPROFILE)/Desktop/output.json" 
+$output_file = "$($env:USERPROFILE)/Desktop/transcript.csv" 
+
+# remove file if it exists
+if (Test-Path -Path $output_file) {
+    Remove-Item -Path $output_file
+}
+
+# get Google Cloud output
+$json = Get-Content $input_file | ConvertFrom-Json 
+
+# extract text
+foreach ($item in $json.results) {
+    $obj = New-Object PSObject -Property @{
+        'confidence'=$item.alternatives.confidence;
+        'transcript'=$item.alternatives.transcript 
+    }
+    $obj| Export-Csv -Path $output_file -NoTypeInformation -Append
+}
